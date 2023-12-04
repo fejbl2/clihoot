25e75f0c715d228e7e3fa8f1dcf0bb1b424b49a4
Robert Gemrot <robert.gemrot@centrum.cz>
2023-12-04 14:58:30 +0100

    feat: Design folder structure

---
+++ client/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let x = common::Hello::World;
+    println!("Hello, world!");
+}
---
+++ server/src/main.rs
@@ -0,0 +1,6 @@
+use common::Hello;
+
+fn main() {
+    let x = Hello::World;
+    println!("Hello, world!");
+}
