6c99483551e06285aae1175e5000fc8b995ba557
Peter Romančík <xromanc1@fi.muni.cz>
2023-12-11 15:59:38 +0100

    feat: add messages for terminal actors

---
+++ common/src/terminal/messages.rs
@@ -0,0 +1,16 @@
+use actix::prelude::*;
+use crossterm::event::KeyCode;
+
+#[derive(Message)]
+#[rtype(result = "anyhow::Result<()>")]
+pub struct Stop;
+
+#[derive(Message)]
+#[rtype(result = "anyhow::Result<()>")]
+pub struct Redraw;
+
+#[derive(Message)]
+#[rtype(result = "anyhow::Result<()>")]
+pub struct KeyPress {
+    pub key_code: KeyCode,
+}
