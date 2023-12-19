c34bd3b79fe8e992652006e2dcce8404985feeae
Robert Gemrot <robert.gemrot@centrum.cz>
2023-12-19 17:08:06 +0100

    fix: remove ignore

--- server/tests/question_ends_itself_after_timeout.rs
+++ server/tests/question_ends_itself_after_timeout.rs
@@ -15,7 +15,6 @@ use crate::fixtures::create_server::create_server;
 use crate::fixtures::create_server_and_teacher::create_server_and_teacher;
 
 #[rstest]
-#[ignore]
 #[tokio::test]
 #[timeout(Duration::from_secs(10))]
 async fn question_ends_itself_after_timeout_test_implementation() -> anyhow::Result<()> {
