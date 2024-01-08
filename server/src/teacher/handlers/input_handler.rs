d92937b371eb022fcf4fb6a82c19c2f256e7cb14
Peter Romančík <xromanc1@fi.muni.cz>
2024-01-08 21:05:57 +0100

    feat(#38): allow teacher to kick students

--- server/src/teacher/handlers/input_handler.rs
+++ server/src/teacher/handlers/input_handler.rs
@@ -5,7 +5,7 @@ use log::debug;
 use ratatui::widgets::ListState;
 
 use crate::{
-    messages::lobby::{EndQuestion, StartQuestion, SwitchToLeaderboard},
+    messages::lobby::{EndQuestion, KickPlayer, StartQuestion, SwitchToLeaderboard},
     teacher::terminal::{TeacherTerminal, TeacherTerminalState},
 };
 
@@ -16,14 +16,40 @@ impl TerminalHandleInput for TeacherTerminal {
             TeacherTerminalState::StartGame {} => {
                 if key_code == KeyCode::Enter {
                     self.state = TeacherTerminalState::WaitingForGame {
-                        list_state: ListState::default(),
+                        list_state: ListState::default().with_selected(Some(0)),
                     };
                 }
             }
-            TeacherTerminalState::WaitingForGame { list_state: _ } => {
-                if key_code == KeyCode::Enter {
-                    self.lobby.do_send(StartQuestion);
-                }
+            TeacherTerminalState::WaitingForGame { list_state } => {
+                let mut selected = list_state.selected().unwrap_or(0);
+
+                match key_code {
+                    KeyCode::Enter => self.lobby.do_send(StartQuestion),
+                    KeyCode::Down | KeyCode::Char('j' | 's') => {
+                        selected += 1;
+                        if selected >= self.players.len() {
+                            selected = 0;
+                        }
+                        list_state.select(Some(selected));
+                    }
+                    KeyCode::Up | KeyCode::Char('k' | 'w') => {
+                        if selected == 0 {
+                            selected = self.players.len() - 1;
+                        } else {
+                            selected -= 1;
+                        }
+                        list_state.select(Some(selected));
+                    }
+                    KeyCode::Char('x') => {
+                        let selected = list_state.selected().unwrap_or(0);
+
+                        self.lobby.do_send(KickPlayer {
+                            player_uuid: self.players[selected].uuid,
+                            reason: None,
+                        });
+                    }
+                    _ => {}
+                };
             }
             TeacherTerminalState::Question {
                 question: q,
@@ -46,12 +72,38 @@ impl TerminalHandleInput for TeacherTerminal {
                 }
             }
             TeacherTerminalState::Results {
-                results: _,
-                list_state: _,
+                results,
+                list_state,
             } => {
-                if key_code == KeyCode::Enter {
-                    self.lobby.do_send(StartQuestion);
-                }
+                let mut selected = list_state.selected().unwrap_or(0);
+
+                match key_code {
+                    KeyCode::Enter => self.lobby.do_send(StartQuestion),
+                    KeyCode::Down | KeyCode::Char('j' | 's') => {
+                        selected += 1;
+                        if selected >= results.players.len() {
+                            selected = 0;
+                        }
+                        list_state.select(Some(selected));
+                    }
+                    KeyCode::Up | KeyCode::Char('k' | 'w') => {
+                        if selected == 0 {
+                            selected = results.players.len() - 1;
+                        } else {
+                            selected -= 1;
+                        }
+                        list_state.select(Some(selected));
+                    }
+                    KeyCode::Char('x') => {
+                        let selected = list_state.selected().unwrap_or(0);
+
+                        self.lobby.do_send(KickPlayer {
+                            player_uuid: results.players[selected].0.uuid,
+                            reason: None,
+                        });
+                    }
+                    _ => {}
+                };
             }
             TeacherTerminalState::EndGame {
                 list_state: _,
--- server/src/teacher/handlers/show_leaderboard_handler.rs
+++ server/src/teacher/handlers/show_leaderboard_handler.rs
@@ -10,9 +10,12 @@ impl TerminalHandleShowLeaderboard for TeacherTerminal {
     fn handle_show_leaderboard(&mut self, show: ShowLeaderboard) -> anyhow::Result<()> {
         debug!("Teacher: handling show leaderboard");
 
+        let mut list_state = ListState::default();
+        list_state.select(Some(0));
+
         self.state = TeacherTerminalState::Results {
             results: show,
-            list_state: ListState::default(),
+            list_state,
         };
 
         Ok(())
